b33a37be5227c088bd316456435dd51c95926fbe
LucasLLimeira <Lucasdllimeira@hotmail.com>
2024-04-02 22:08:06 -0300

    header

--- angular/my-app/src/app/components/header/header.component.ts
+++ angular/my-app/src/app/components/header/header.component.ts
@@ -1,8 +1,10 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component, Input, OnInit, input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
+  imports: [NgOptimizedImage],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
