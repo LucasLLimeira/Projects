7b5fe55c4f0cb14f40eac1f8bfe4f218b1987092
LucasLLimeira <Lucasdllimeira@hotmail.com>
2024-04-03 13:09:38 -0300

    tailwind

---
+++ projeto react/pass-in-web/src/App.tsx
@@ -0,0 +1,20 @@
+interface MeuBotaoProps {
+  texto: string
+}
+
+function MeuBotao(props: MeuBotaoProps){
+  return <button className="bg-orange-400 h-10 px-3 rounded font-medium">{props.texto}</button>
+}
+
+
+ export function App() {
+  return (
+    <div className="flex gap-2">
+      <MeuBotao texto="Clique aqui" />
+      <MeuBotao texto="Botão 2" />
+
+
+    </div>
+  )
+}
+
